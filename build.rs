270d69f301ddd5e99cf1ed23af2744f90c535fea
Alexander Irbis <irbis.labs@gmail.com>
2017-07-06 00:40:46 +0000

    cleanup build.rs

--- build.rs
+++ build.rs
@@ -1,25 +1,17 @@
-use std::env;
 use std::fs::File;
 use std::io::Write;
-use std::path::Path;
 use std::process::Command;
-use std::str::from_utf8;
-
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let out_dir = Path::new(&out_dir);
-
     let mut f = File::create("src/release.rs").unwrap();
 
     {
-        let version = match Command::new("rustc")
+        let version = Command::new("rustc")
             .arg("--version")
-            .output() {
-                Ok(o) => String::from(from_utf8(&o.stdout).unwrap().trim()),
-                Err(_) => String::new(),
-            };
+            .output()
+            .map(|o| String::from_utf8(o.stdout).unwrap())
+            .expect("Can't get rustc version")
+        ;
         writeln!(f, r"pub const RUSTC_VERSION: &'static str = {:?};", version).unwrap();
-        writeln!(f, r"pub const OUT_DIR: &'static str = {:?};", out_dir).unwrap();
     }
 }
